694f61a3d8bea95b90e7fcd6c05d94fa822a4e1e
david dali susanibar arce <davi.sarces@gmail.com>
2022-11-09 13:25:23 -0500

    feat: build to sonatype

--- ci/release/publish.sh
+++ ci/release/publish.sh
@@ -4,6 +4,4 @@
 set -euo pipefail
 
 gradle wrapper
-#./gradlew clean publishToSonatype closeSonatypeStagingRepository
-
-./gradlew clean publishToSonatype -PSONATYPE_USER=$SONATYPE_USER -PSONATYPE_PASSWORD=$SONATYPE_PASSWORD -PSIGNING_KEY_ID=$SIGNING_KEY_ID -PSIGNING_PASSWORD=$SIGNING_PASSWORD -PSIGNING_KEY=$SIGNING_KEY
+./gradlew clean :isthmus:publishToSonatype :isthmus:publishToSonatype :isthmus:closeSonatypeStagingRepository :core:closeSonatypeStagingRepository
