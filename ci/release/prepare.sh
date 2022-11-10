34322f26e901f62fe80da6d57c33f8ffc5e1d712
david dali susanibar arce <davi.sarces@gmail.com>
2022-11-10 08:48:47 -0500

    feat: test feat version increase 4th one

--- ci/release/prepare.sh
+++ ci/release/prepare.sh
@@ -12,5 +12,6 @@ sed -ir "s/^version = .*/version = $1/" gradle.properties
 ls -latr isthmus/build/graal
 mv isthmus/build/graal/isthmus-macOS-latest isthmus/build/graal/isthmus-macOS-latest-$1
 mv isthmus/build/graal/isthmus-ubuntu-latest isthmus/build/graal/isthmus-ubuntu-latest-$1
+ls -latr isthmus/build/graal
 
 
